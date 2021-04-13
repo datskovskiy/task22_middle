f19552f439ac4fc51818906450259c4a7753b953
anton.datskovskiy <datskovskiy.anton@gmail.com>
2021-04-13 10:09:17 +0300

    Implement task

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const submitBtn = document.getElementById('submitBtn');
-
-submitBtn.onclick = function () {
+function createTable () {
   const rows = document.getElementById('row').value;
   const cols = document.getElementById('column').value;
   if (rows < 0 || cols < 0)
@@ -14,6 +12,7 @@ submitBtn.onclick = function () {
   divTable.id = 'table';
 
   let table = document.createElement('table');
+  table.id = 'data';
   for (let i = 0; i < rows; i++) {
     let tr = document.createElement('tr');
     table.append(tr);
@@ -26,4 +25,31 @@ submitBtn.onclick = function () {
 
   document.getElementById('middle').append(divTable);
   divTable.append(table);
-};
+}
+
+function fillTable() {
+  let table = document.getElementById('data');
+  if (table === null) 
+    return;
+
+  for (let r = 0; r < table.childNodes.length; r++) {
+    const tr = table.childNodes[r];
+    for (let c = 0; c < tr.childNodes.length; c++) {
+      const td = tr.childNodes[c];
+      td.innerHTML = `${r + 1}${c + 1}`;
+
+      td.addEventListener('click', function() {
+        td.style.backgroundColor = (td.style.backgroundColor === '') ? '#' + getRandomColor() : '';      
+      });   
+    }  
+  }
+}
+
+function getRandomColor() {
+  return Math.floor(Math.random() * 16777215).toString(16);
+}
+
+const submitBtn = document.getElementById('submitBtn');
+
+submitBtn.addEventListener('click', createTable);
+submitBtn.addEventListener('click', fillTable);
